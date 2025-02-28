045059ccfe0ef3b63224e2aee5802c4367fe2698
Andriy Gnap <dghjjkk63@gmail.com>
2025-02-28 09:09:19 +0200

    Added model,migration Modelauto and relationship between model Category-Modelauto

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,9 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    public function products()
-    {
-        // Зв'язок - Категорія може мати багато продуктів
-        return $this->hasMany(Product::class, 'category_id');
-    }
+	public function products()
+	{
+		// Зв'язок - Категорія може мати багато продуктів
+		return $this->hasMany(Product::class, 'category_id');
+	}
+
+	public function modelautos()
+	{
+		return $this->hasMany(Modelauto::class, 'category_id');
+	}
 }
---
+++ database/migrations/2025_02_27_183143_create_modelautos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('modelautos', function (Blueprint $table) {
+					$table->id();
+					$table->unsignedBigInteger('category_id')->nullable()->index();
+					$table->string('name');
+					$table->string('image_path')->nullable();
+					$table->boolean('status')->default(1);
+					$table->integer('start_year')->nullable();
+					$table->integer('end_year')->nullable();
+					$table->timestamps();
+
+					$table->foreign('category_id')
+						->references('id')
+						->on('categories')
+						->cascadeOnDelete()
+						->cascadeOnUpdate();
+					;
+				});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('modelautos');
+    }
+};
