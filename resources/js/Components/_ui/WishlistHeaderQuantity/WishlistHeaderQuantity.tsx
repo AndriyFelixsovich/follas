ba94dcef7d0345ebc05d82211494f5459a1f8e53
Vitalii Stetsyk <vitalik.sts@gmail.com>
2025-04-08 13:10:00 +0300

    Wishlist quntity #1

--- resources/js/Components/_ui/WishlistHeaderQuantity/WishlistHeaderQuantity.tsx
+++ resources/js/Components/_ui/WishlistHeaderQuantity/WishlistHeaderQuantity.tsx
@@ -3,9 +3,11 @@ import { usePage } from '@inertiajs/react';
 import styles from "./style.module.scss";
 
 const WishlistHeaderQuantity: FC = () => {
+	const { productsInWishlist } = usePage().props;
+	const wishlistCount = productsInWishlist ? productsInWishlist.length : 0;
 
 	return (
-		<div className={styles.total_quantity}>0</div>
+		<div className={styles.total_quantity}>{wishlistCount}</div>
 	)
 };
 
