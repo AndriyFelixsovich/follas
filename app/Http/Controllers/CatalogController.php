c7521d7a62c3026a0b1dbdc2fabf2e85ab7aefba
Andriy Gnap <dghjjkk63@gmail.com>
2025-04-10 10:31:58 +0300

    Added Cache for CatalogController

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -3,11 +3,16 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\MarkaAutoResource;
 use App\Models\MarkaAuto;
+use Illuminate\Support\Facades\Cache;
+use Inertia\Response as InertiaResponse;
 use Inertia\Inertia;
 
 class CatalogController extends Controller {
-	public function index() {
-		$markaAll = MarkaAuto::where('status', '=', '1')->get();
+	public function index(): InertiaResponse
+	{
+		$markaAll = Cache::remember('marka_all_active', 60 * 60, function () {
+			return MarkaAuto::where('status', '=', '1')->get();
+		});
 
 		return Inertia::render('Page/Catalog', [
 			'category' => MarkaAutoResource::collection($markaAll)->resolve()
