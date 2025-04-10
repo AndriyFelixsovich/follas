c9d04ea485b69464b5d47bb9ad0450803fc5c89d
Andriy Gnap <dghjjkk63@gmail.com>
2025-04-10 10:35:35 +0300

    Added underline link for page in header

--- resources/js/Components/HeaderNav/HeaderNav.tsx
+++ resources/js/Components/HeaderNav/HeaderNav.tsx
@@ -6,10 +6,10 @@ const HeaderNav: FC = () => {
   return (
 	 <nav className={styles.nav}>
 		 <ul>
-			 <li><Link href={route('catalog.index')}>Catalog</Link></li>
-			 <li><Link href={route('blog.index')}>Blog</Link></li>
-			 <li><Link href={route('about.index')}>About Us</Link></li>
-			 <li><Link href={route('contacts.index')}>Contact</Link></li>
+			 <li><Link className={route().current('catalog.index') ? 'font-bond underline' : ''} href={route('catalog.index')}>Catalog</Link></li>
+			 <li><Link className={route().current('blog.index') ? 'font-bond underline' : ''} href={route('blog.index')}>Blog</Link></li>
+			 <li><Link className={route().current('about') ? 'font-bond underline' : ''} href={route('about')}>About Us</Link></li>
+			 <li><Link className={route().current('contacts.index') ? 'font-bond underline' : ''} href={route('contacts.index')}>Contact</Link></li>
 		 </ul>
 	 </nav>
   );
