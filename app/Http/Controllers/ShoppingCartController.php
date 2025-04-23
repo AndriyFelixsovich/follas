8c459728104f9d92800ea9f71e52113f022359bd
Andriy Gnap <dghjjkk63@gmail.com>
2025-04-23 12:27:22 +0300

    Added product in ShoppingCart

--- app/Http/Controllers/ShoppingCartController.php
+++ app/Http/Controllers/ShoppingCartController.php
@@ -1,10 +1,25 @@
 <?php
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Illuminate\Support\Facades\Session;
 use Inertia\Inertia;
 
 class ShoppingCartController extends Controller {
     public function index() {
-       return Inertia::render('Page/ShoppingCart');
+
+			$products = $productIds = [];
+
+			if (session()->has('cart') && is_object(session('cart')) && !empty((array) session('cart'))) {
+				$productIds = session()->get('cart.product_id', []);
+				$products = Product::where('status', '=', 1)
+					->whereIn('id', $productIds)
+					->get();
+			}
+
+
+			return Inertia::render('Page/ShoppingCart', [
+				 'product' => $products,
+			 ]);
     }
 }
