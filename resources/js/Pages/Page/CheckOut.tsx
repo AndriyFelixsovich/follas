0d86ae6cdc18b3187b33e20dc6bd85547d5f349b
Vitalii Stetsyk <vitalik.sts@gmail.com>
2025-04-25 15:16:24 +0300

    Checkout fix #1

--- resources/js/Pages/Page/CheckOut.tsx
+++ resources/js/Pages/Page/CheckOut.tsx
@@ -1,5 +1,5 @@
-import {  FC, useState  } from "react";
-import {  Head, usePage, useForm  } from '@inertiajs/react';
+import { FC } from "react";
+import { Head, useForm } from '@inertiajs/react';
 import styles from './checkout.module.scss';
 import Container from '@/Components/_ui/Container/Container';
 import MainLayout from "@/Layouts/MainLayout";
@@ -12,25 +12,20 @@ import OrderItem from '@/Components/OrderItem/OrderItem';
 import PrimaryButton from '@/Components/_ui/PrimaryButton/PrimaryButton';
 
 const CheckOut: FC = () => {
-	const [formData, setFormData] = useState({
+	const { data, setData, post, errors } = useForm({
 		name: '',
 		phone: '',
 		email: '',
 		comment: ''
 	});
 
-	const handleChange = (e) => {
-		const { name, value } = e.target;
-		setFormData((prev) => ({
-			...prev,
-			[name]: value
-		}));
-	};
+	const handleChange = e => setData(e.target.name, e.target.value);
 
-	const formSubmitHandler = (e) => {
+	const formSubmitHandler = e => {
 		e.preventDefault();
-		console.log(formData);
-	}
+		// post('/checkout');
+		console.log(data)
+	};
 
 	return (
 		<MainLayout>
@@ -46,34 +41,39 @@ const CheckOut: FC = () => {
 							<form onSubmit={formSubmitHandler}>
 								<div className={styles.form_field}>
 									<InputLabel htmlFor="name" value="Name"/>
-									<TextInput type="text" id="name" name="name" value={formData.name} onChange={handleChange} />
+									<TextInput type="text" id="name" name="name" value={data.name} onChange={handleChange} />
+									{errors.name && <InputError message={errors.name} />}
 								</div>
 								<div className={styles.form_field}>
 									<InputLabel htmlFor="phone" value="Phone"/>
-									<TextInput type="text" id="phone" name="phone" value={formData.phone} onChange={handleChange} />
+									<TextInput type="text" id="phone" name="phone" value={data.phone} onChange={handleChange}/>
+									{errors.phone && <InputError message={errors.phone} />}
 								</div>
 								<div className={styles.form_field}>
 									<InputLabel htmlFor="email" value="Email"/>
-									<TextInput type="text" id="email" name="email" value={formData.email} onChange={handleChange} />
+									<TextInput type="text" id="email" name="email" value={data.email} onChange={handleChange}/>
+									{errors.email && <InputError message={errors.email} />}
 								</div>
 								<div className={styles.form_field}>
 									<InputLabel htmlFor="comment" value="Comment"/>
-									<Textarea id="comment"  name="comment" value={formData.comment} onChange={handleChange} />
+									<Textarea id="comment" name="comment" value={data.comment} onChange={handleChange}/>
+									{errors.comment && <InputError message={errors.comment} />}
+								</div>
+
+								<div className={styles.form_field_send}>
+									<PrimaryButton onClick={formSubmitHandler}>Send</PrimaryButton>
 								</div>
 							</form>
-
-							<div className={styles.form_field_send}>
-								<PrimaryButton onClick={formSubmitHandler}>Send</PrimaryButton>
-							</div>
 						</div>
 						<div>
 							<h3>Product order</h3>
-								<OrderItem />
+							<OrderItem />
 						</div>
 					</div>
 				</Container>
 			</div>
-		</MainLayout>);
+		</MainLayout>
+	);
 }
 
 export default CheckOut;
