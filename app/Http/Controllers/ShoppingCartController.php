a39beed159422fa07a59842e7bac65fe5f111747
Andriy Gnap <dghjjkk63@gmail.com>
2025-04-28 16:38:58 +0300

    Added in cart totalPrice

--- app/Http/Controllers/ShoppingCartController.php
+++ app/Http/Controllers/ShoppingCartController.php
@@ -13,30 +13,42 @@ class ShoppingCartController extends Controller {
 			$products = collect();
 
 			if (auth()->check()) {
+				$cartItems = ShoppingCart::query()
+					->where('user_id', auth()->id())
+					->with('product')
+					->get();
 
-				$cartItems = ShoppingCart::query()->where('user_id', auth()->id())->with('product')->get();
 				$products = $cartItems->map(function ($item) {
 					$item->product->quantity = $item->quantity;
 					return $item->product;
 				});
-
 			} else {
 				$cartItems = session()->get('cart', []);
 
 				if (!empty($cartItems) && is_array($cartItems)) {
 					$productIds = array_column($cartItems, 'product_id');
-					$products = Product::where('is_published', '=', 1)
+
+					$products = Product::where('is_published', 1)
 						->whereIn('id', $productIds)
 						->get()
 						->each(function ($product) use ($cartItems) {
 							$cartItem = collect($cartItems)->firstWhere('product_id', $product->id);
-							$product->quantity = $cartItem ? $cartItem['quantity'] : 0;
+							if ($cartItem) {
+								$product->quantity = $cartItem['quantity'];
+							} else {
+								$product->quantity = 0;
+							}
 						});
 				}
 			}
 
+			$totalPrice = $products->sum(function ($product) {
+				return ($product->price ?? 0) * ($product->quantity ?? 0);
+			});
+
 			return Inertia::render('Page/ShoppingCart', [
-				'products' => ProductsAutoResource::collection($products)
+				'products' => ProductsAutoResource::collection($products),
+				'totalPrice' => $totalPrice
 			]);
     }
 }
