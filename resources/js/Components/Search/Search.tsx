c064a12bf2b5161b1932a60b1cb7ff9d66c1affb
Vitalii Stetsyk <vitalik.sts@gmail.com>
2025-05-14 13:34:43 +0300

    Add serach result #2

--- resources/js/Components/Search/Search.tsx
+++ resources/js/Components/Search/Search.tsx
@@ -7,32 +7,32 @@ import styles from './style.module.scss';
 const Search: FC = () => {
 	const [inputError, setInputError] = useState(false);
 
-	const { data, setData, post } = useForm({
+	const searchForm = useForm({
 		value: ''
 	});
 
 	const onInputHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setData('value', e.target.value);
+		searchForm.setData('value', e.target.value);
 	};
 
 	const sendForm = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 
-		if(!data.value) {
+		if (!searchForm.data.value) {
 			setInputError(true);
+			return;
 		}
 
-		post('/SearchResultController/index', {
+		searchForm.post('/SearchResultController/index', {
 			onSuccess: () => {
 				console.log('Send!');
 			}
 		});
-
 	};
 
 	return (
 		<form className={styles.form} onSubmit={sendForm}>
-			<Input placeholder="Search" onInputHandler={onInputHandler} inputValue={data.value} error={inputError} />
+			<Input placeholder="Search" onInputHandler={onInputHandler} inputValue={searchForm.data.value} error={inputError} />
 			<button type="submit">
 				<SearchIcon width="27" height="27" fill="#2e3b4c" />
 			</button>
